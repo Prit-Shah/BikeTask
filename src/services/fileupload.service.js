e619fe33cbe6fadea43bae62fca287d3f8e84ce7
Prit-Shah <shahprit8866@gmail.com>
2023-02-17 14:16:19 +0530

    with cloudinary

--- src/services/fileupload.service.js
+++ src/services/fileupload.service.js
@@ -1,23 +1,52 @@
 var multer = require('multer');
 const fs = require('fs')
 const path = require('path')
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: "dycfoq3tx",
+    api_key: "826762248647563",
+    api_secret: "vSPygPEZcfC2MLNTPbIJHNMjS9k"
+});
+let fileName = "";
 var upload = multer({
+
     storage: multer.diskStorage({
         destination: (req, file, cb) => {
-            if (req.path === '/signup') {
-                const stpath = "./public/uploads/users";
-                fs.mkdirSync(stpath, { recursive: true })
-                cb(null, stpath)
-            }
-            else if (req.path === '/') {
-                const stpath = "./public/uploads/bikes";
-                fs.mkdirSync(stpath, { recursive: true })
-                cb(null, stpath)
-            }
+            // if (req.path === '/signup') {
+            //     const stpath = "./public/uploads/users";
+            //     fs.mkdirSync(stpath, { recursive: true })
+            //     cb(null, stpath)
+            // }
+            // else if (req.path === '/') {
+            //     const stpath = "./public/uploads/bikes";
+            //     fs.mkdirSync(stpath, { recursive: true })
+            //     cb(null, stpath)
+            // }
+            fileName = file.fieldname + '-' + Date.now() + path.extname(file.originalname);
+            const res = cloudinary.uploader.upload(file, { public_id: fileName })
+
+            res.then((data) => {
+                console.log(data);
+                console.log(data.secure_url);
+            }).catch((err) => {
+                console.log(err);
+            });
+
+            // Generate 
+            const url = cloudinary.url(fileName, {
+                width: 100,
+                height: 150,
+                Crop: 'fill'
+            });
+
+            // The output url
+            console.log(url);
+            fileName = url;
         },
         filename: (req, file, cb) => {
             // console.log(path.extname(file.originalname))
-            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+            cb(null, fileName);
         },
 
 
@@ -32,4 +61,5 @@ var upload = multer({
         cb(null, true)
     },
 }).single("photo");
+
 module.exports = upload;
